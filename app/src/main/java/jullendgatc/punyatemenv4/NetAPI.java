002fffd3df3ac02e103ee384aac954adcca7e9e4
dimas <dimasz_97@yahoo.com>
2018-03-05 04:56:56 +0700

    Migration Server to PT Server

--- app/src/main/java/jullendgatc/punyatemenv4/NetAPI.java
+++ app/src/main/java/jullendgatc/punyatemenv4/NetAPI.java
@@ -5,8 +5,8 @@ package jullendgatc.punyatemenv4;
  */
 
 public class NetAPI {
-    public static final String URL = "http://pt.himsigalaksi.com/index.php/";
-    //    public static final String URL = "http://pt.punyatemen.com/index.php/";
+//    public static final String URL = "http://pt.himsigalaksi.com/index.php/";
+    public static final String URL = "http://pt.punyatemen.com/index.php/";
     public static final String GET_BARANG_SEWA_BY_USER_PENYEWA_ID = URL + "getBarangSewaByUserPenyewaId/";
     public static final String GET_PERMINTAAN_BARANG = URL + "getPermintaanBarang";
 }
